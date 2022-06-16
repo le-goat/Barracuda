1bf276c010e16af71e23b36b69eb4e53669b048b
congiu.gaetan <gaetan.congiu@esqese.ucly.fr>
2022-06-16 21:59:07 +0200

    Creation de la fonction updateAUSER

--- application/sources/requests/Update.tsx
+++ application/sources/requests/Update.tsx
@@ -2,4 +2,21 @@
 // UpdateFavoris(user_id, bar_id) -> Quand l'utilisateur ajoute un favoris
 
 // UpdateNote(bar_id, note) -> Quand l'utilisateur donne une note
+import axios from "axios";
 
+export const updateAUser = (newUsername:string, newEmail:string, token:string) => {
+    return axios
+        .put('http://localhost:1337/api/users/7', {
+            headers: {
+                Authorization:`Bearer ${token}`,
+            },
+            data:{
+                username: newUsername,
+                email: newEmail,
+            }
+        })
+        .then(response => (console.log(response)))
+        .catch((error) => {
+            console.log('An error occurred:', error.response);
+        })
+}
