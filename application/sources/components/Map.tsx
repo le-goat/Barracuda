d8a29ee65e92130494417009effc632d4eb00ed1
le-goat <58209708+le-goat@users.noreply.github.com>
2022-06-16 09:45:35 +0200

    Erreur avec mapbox

--- application/sources/components/Map.tsx
+++ application/sources/components/Map.tsx
@@ -1,16 +1,53 @@
 // La carte interactive -> Prends en paramètre une liste de bar et doit trouver la position de l'utilisateur
 import {View} from "react-native";
+import MapboxGL from '@react-native-mapbox-gl/maps';
+// import MapView, { Marker } from "react-native-maps";
+// import MapboxGL from "@rnmapbox/maps";
+import React, {useState} from "react";
+import {Component} from "react";
+// import MapView from 'react-native-maps';
 
-const Map = () => {
+MapboxGL.setAccessToken('pk.eyJ1IjoibGUtZ29hdCIsImEiOiJjbDRmOXJ6ZnMwMG5rM2RzaHg4NGh2bHdiIn0.GT9RNz1K1DkL_PzKiuFTlg');
+MapboxGL.setConnected(true);
+// console.disableYellowBox = true; // Pas sur de ça  #Manu : je pense pas non plus
+
+
+/*export default function Map() {
+    return(
+        <View>
+            <MapView/>
+        </View>
+    )
+}*/
+
+
+export const Map = () => {
+    const coordinates = useState([])
+
     return (
-        <View
-            style={{
-
-            }}
-        >
-
+        <View style={styles.MainContainer}>
+            <View style={styles.SubContainer}>
+                <MapboxGL.MapView style={styles.Mapbox}>
+                    <MapboxGL.Camera zoomLevel={9} centerCoordinate={coordinates}/>
+                    <MapboxGL.PointAnnotation id={1} coordinate={coordinates}/>
+                </MapboxGL.MapView>
+            </View>
         </View>
     )
 }
 
-export default Map;
+const styles = StyleSheet.create({
+    MainContainer: {
+        flex: 1,
+    },
+    SubContainer: {
+        height: '100%',
+        width: '100%',
+        backgroundColor: 'white',
+    },
+    Mapbox: {
+        flex: 1,
+    },
+});
+
+// export default Map;
--- application/sources/screens/HomePage.tsx
+++ application/sources/screens/HomePage.tsx
@@ -1,1 +1,64 @@
-// La carte centrée sur la position de l'utilisateurs avec tous les bars représentés
+import {StyleSheet, View, Button, Image} from "react-native";
+import ResearchButton from "../components/ResearchButton";
+import {FunctionComponent} from "react";
+import {NavigationContainer, NavigationProp, RouteProp} from "@react-navigation/native";
+import Login from "../components/Login";
+import {StatusBar} from "expo-status-bar";
+import * as React from "react";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {RootStackParamList} from "../../RootStackParamList";
+import BarPreview from "../components/BarPreview";
+import {Map} from "../components/Map";
+
+
+type Props = {
+    navigation: NavigationProp<RootStackParamList>;
+}
+
+
+
+export const HomePage: FunctionComponent<Props> = ({navigation}) => {
+
+    const bar_id : number = 1;
+    return (
+        <View style={styles.container}>
+
+
+            <View
+                style={styles.viewbutton}>
+                <Button
+                    title="Login"
+
+                    onPress={() =>
+                        navigation.navigate('Login')
+
+                    }/>
+            </View>
+
+
+            <ResearchButton/>
+
+            <BarPreview id={1}/>
+
+            {/*<Map/>*/}
+
+            <StatusBar style="auto"/>
+        </View>
+
+
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+
+    },
+    viewbutton: {
+        borderWidth: 2,
+        borderColor: 'black',
+        width: '100%',
+    },
+});
